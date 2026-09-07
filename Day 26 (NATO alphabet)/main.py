c0a1506c89a366396218a329e1bad51d48e8b4cc
xvinkage <69357215+xvinkage@users.noreply.github.com>
2026-09-07 08:25:33 -0400

    add handling error and exception

--- Day 26 (NATO alphabet)/main.py
+++ Day 26 (NATO alphabet)/main.py
@@ -6,10 +6,17 @@ nato_df = pd.DataFrame(data)
 nato_dict = {row["letter"]: row["code"] for (index, row) in nato_df.iterrows()}
 # print(nato_dict)
 
-user_input = input("Enter a word: ").upper()
+def generate_nato():
+    user_input = input("Enter a word: ").upper()
+    try:
+        letters = [letter for letter in user_input]
+        # print(letters)
 
-letters = [letter for letter in user_input]
-# print(letters)
+        nato = [nato_dict[letter] for letter in letters]
+        print(nato)
+    except KeyError: 
+        print("Please only enter valid letters")
+        generate_nato()
 
-nato = [nato_dict[letter] for letter in letters if nato_dict[letter]]
-print(nato)
+
+generate_nato()
